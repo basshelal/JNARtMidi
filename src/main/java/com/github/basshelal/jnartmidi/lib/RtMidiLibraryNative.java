0f1d90abb28dd54dc39ef07771e125c9e26eb2f2
Bassam Helal <basshelal@gmail.com>
2021-02-15 04:54:19 +0000

    Preliminary progress, JNAerator gave us something decent

--- src/main/java/com/github/basshelal/jnartmidi/lib/RtMidiLibraryNative.java
+++ src/main/java/com/github/basshelal/jnartmidi/lib/RtMidiLibraryNative.java
@@ -1,6 +1,11 @@
 package com.github.basshelal.jnartmidi.lib;
 
 import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
 public class RtMidiLibraryNative implements RtMidiLibrary {
 
@@ -8,6 +13,100 @@ public class RtMidiLibraryNative implements RtMidiLibrary {
         Native.register(RtMidiLibrary.LIBRARY_NAME);
     }
 
-    // TODO: 15/02/2021 Add all functions from RtMidiLibrary as native
+    @Override
+    public native int rtmidi_get_compiled_api(IntByReference apis, int apis_size);
+
+    @Override
+    public native int rtmidi_get_compiled_api(IntBuffer apis, int apis_size);
+
+    @Override
+    public native String rtmidi_api_name(int api);
+
+    @Override
+    public native String rtmidi_api_display_name(int api);
+
+    @Override
+    public native int rtmidi_compiled_api_by_name(Pointer name);
+
+    @Override
+    public native int rtmidi_compiled_api_by_name(String name);
+
+    @Override
+    public native void rtmidi_error(int type, Pointer errorString);
+
+    @Override
+    public native void rtmidi_error(int type, String errorString);
+
+    @Override
+    public native void rtmidi_open_port(RtMidiWrapper device, int portNumber, Pointer portName);
+
+    @Override
+    public native void rtmidi_open_port(RtMidiWrapper device, int portNumber, String portName);
+
+    @Override
+    public native void rtmidi_open_virtual_port(RtMidiWrapper device, Pointer portName);
+
+    @Override
+    public native void rtmidi_open_virtual_port(RtMidiWrapper device, String portName);
+
+    @Override
+    public native void rtmidi_close_port(RtMidiWrapper device);
+
+    @Override
+    public native int rtmidi_get_port_count(RtMidiWrapper device);
+
+    @Override
+    public native String rtmidi_get_port_name(RtMidiWrapper device, int portNumber);
+
+    @Override
+    public native RtMidiWrapper rtmidi_in_create_default();
+
+    @Override
+    public native RtMidiWrapper rtmidi_in_create(int api, Pointer clientName, int queueSizeLimit);
+
+    @Override
+    public native RtMidiWrapper rtmidi_in_create(int api, String clientName, int queueSizeLimit);
+
+    @Override
+    public native void rtmidi_in_free(RtMidiWrapper device);
+
+    @Override
+    public native int rtmidi_in_get_current_api(RtMidiWrapper device);
+
+    @Override
+    public native void rtmidi_in_set_callback(RtMidiWrapper device, RtMidiCCallback callback, Pointer userData);
+
+    @Override
+    public native void rtmidi_in_cancel_callback(RtMidiWrapper device);
+
+    @Override
+    public native void rtmidi_in_ignore_types(RtMidiWrapper device, byte midiSysex, byte midiTime, byte midiSense);
+
+    @Override
+    public native double rtmidi_in_get_message(RtMidiWrapper device, Pointer message, NativeSizeByReference size);
+
+    @Override
+    public native double rtmidi_in_get_message(RtMidiWrapper device, ByteBuffer message, NativeSizeByReference size);
+
+    @Override
+    public native RtMidiWrapper rtmidi_out_create_default();
+
+    @Override
+    public native RtMidiWrapper rtmidi_out_create(int api, Pointer clientName);
+
+    @Override
+    public native RtMidiWrapper rtmidi_out_create(int api, String clientName);
+
+    @Override
+    public native void rtmidi_out_free(RtMidiWrapper device);
+
+    @Override
+    public native int rtmidi_out_get_current_api(RtMidiWrapper device);
+
+    @Override
+    public native int rtmidi_out_send_message(RtMidiWrapper device, Pointer message, int length);
+
+    @Override
+    public native int rtmidi_out_send_message(RtMidiWrapper device, byte[] message, int length);
 
 }
