49c22535aa8439c2e4cf667e528155e62dca28fb
Bassam Helal <basshelal@gmail.com>
2021-02-16 08:53:13 +0000

    Minor changes and removals of deprecated duplicates, unsure about some type mappings

--- src/main/java/com/github/basshelal/jnartmidi/lib/RtMidiLibraryNative.java
+++ src/main/java/com/github/basshelal/jnartmidi/lib/RtMidiLibraryNative.java
@@ -2,7 +2,6 @@ package com.github.basshelal.jnartmidi.lib;
 
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
-import com.sun.jna.ptr.IntByReference;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
@@ -12,9 +11,6 @@ public class RtMidiLibraryNative implements RtMidiLibrary {
     static {
         Native.register(RtMidiLibrary.LIBRARY_NAME);
     }
-
-    @Override
-    public native int rtmidi_get_compiled_api(IntByReference apis, int apis_size);
 
     @Override
     public native int rtmidi_get_compiled_api(IntBuffer apis, int apis_size);
@@ -38,13 +34,7 @@ public class RtMidiLibraryNative implements RtMidiLibrary {
     public native void rtmidi_error(int type, String errorString);
 
     @Override
-    public native void rtmidi_open_port(RtMidiWrapper device, int portNumber, Pointer portName);
-
-    @Override
     public native void rtmidi_open_port(RtMidiWrapper device, int portNumber, String portName);
-
-    @Override
-    public native void rtmidi_open_virtual_port(RtMidiWrapper device, Pointer portName);
 
     @Override
     public native void rtmidi_open_virtual_port(RtMidiWrapper device, String portName);
@@ -60,9 +50,6 @@ public class RtMidiLibraryNative implements RtMidiLibrary {
 
     @Override
     public native RtMidiWrapper rtmidi_in_create_default();
-
-    @Override
-    public native RtMidiWrapper rtmidi_in_create(int api, Pointer clientName, int queueSizeLimit);
 
     @Override
     public native RtMidiWrapper rtmidi_in_create(int api, String clientName, int queueSizeLimit);
@@ -83,16 +70,10 @@ public class RtMidiLibraryNative implements RtMidiLibrary {
     public native void rtmidi_in_ignore_types(RtMidiWrapper device, byte midiSysex, byte midiTime, byte midiSense);
 
     @Override
-    public native double rtmidi_in_get_message(RtMidiWrapper device, Pointer message, NativeSizeByReference size);
-
-    @Override
     public native double rtmidi_in_get_message(RtMidiWrapper device, ByteBuffer message, NativeSizeByReference size);
 
     @Override
     public native RtMidiWrapper rtmidi_out_create_default();
-
-    @Override
-    public native RtMidiWrapper rtmidi_out_create(int api, Pointer clientName);
 
     @Override
     public native RtMidiWrapper rtmidi_out_create(int api, String clientName);
@@ -102,9 +83,6 @@ public class RtMidiLibraryNative implements RtMidiLibrary {
 
     @Override
     public native int rtmidi_out_get_current_api(RtMidiWrapper device);
-
-    @Override
-    public native int rtmidi_out_send_message(RtMidiWrapper device, Pointer message, int length);
 
     @Override
     public native int rtmidi_out_send_message(RtMidiWrapper device, byte[] message, int length);
